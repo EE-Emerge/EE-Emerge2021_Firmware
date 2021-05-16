395f59fd07063e56c16dc707a956fe086b0f1b3f
Anelise Cho <25128798+anelisecho@users.noreply.github.com>
2021-05-15 23:07:40 -0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,16 +1,5 @@
-/*
-  Blink
-  The basic Energia example.
-  Turns on an LED on for one second, then off for one second, repeatedly.
-  Change the LED define to blink other LEDs.
-  
-  Hardware Required:
-  * LaunchPad with an LED
-  
-  This example code is in the public domain.
-*/
-#include <Wire.h> 
-#include "msp430g2553.h" 
+#include <Wire.h>
+#include "msp430g2553.h"
 
 #define MHZ 1000000
 #define G_LED1 P1_3
@@ -41,10 +30,14 @@
 #define REMAINING_CAPACITY_FILTERED 0x2A
 #define FULL_CHARGE_CAPACITY_UNFILTERED 0x2C
 #define FULL_CHARGE_CAPACITY_FILTERED 0x2E
-#define STATE_OF_CHARGE_UNFILTERED 0x30 
+#define STATE_OF_CHARGE_UNFILTERED 0x30
 #define TRUE_REMAINING_CAPACITY 0x6A
 /************************** GLOBAL VAR START ********************/
+
 int speed = 0;
+bool reached_15 = False; // checks if the battery has reached 15% yet (user is alerted at 15% battery)
+int battery_charge = 0;
+Int new_battery_charge = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -54,104 +47,180 @@ void setup() {
   pinMode(R_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(CTL_FAN, OUTPUT);
-  
+
   pinMode(FAST_BUTTON, INPUT);
   pinMode(SLOW_BUTTON, INPUT);
   pinMode(BATTERY_BUTTON, INPUT);
-  
+
   attachInterrupt(FAST_BUTTON, speedUpInterrupt, RISING);
   attachInterrupt(SLOW_BUTTON, slowDownInterrupt, RISING);
-  attachInterrupt(BATTERY_BUTTON, checkBattery, RISING);
-  
+  attachInterrupt(BATTERY_BUTTON, Battery_Interrupt_Response, RISING);
+
   WDTCTL = WDTPW + WDTHOLD;
-  CCTL0 = CCIE;                             // CCR0 interrupt enabled
+  CCTL0 = CCIE;                             // CCR0 interrupt
   TACTL = TASSEL_2 + MC_1 + ID_3;           // SMCLK/8, upmode  
-  CCR0 =  10000; 
-  P1IE = BATTERY_BUTTON | 1; /* Turn on Interupts for CheckBattery */
- // P1IFG ~= BATTERY_BUTTON; /* Lower Interrupt Flag . I can just set this to zero ???? */P1IES |= BIT3;                     // P1.3 Hi/lo edge
+  CCR0 =  10000;
+  P1IE = BATTERY_BUTTON | 1; /* Turn on Interrupts for CheckBattery */
+  // P1IFG ~= BATTERY_BUTTON; /* Lower Interrupt Flag . I can just set this to zero ???? */P1IES |= BIT3;                     // P1.3 Hi/lo edge
   P1IFG &= ~BIT3;                  // P1.3 IFG cleared
- __bis_SR_register(GIE+CPUOFF); /* GIE stands for Global Interrupt Enable. Enables all Interrupts. Idk if this is necessary */
+  __bis_SR_register(GIE+CPUOFF); /* GIE stands for Global Interrupt Enable. Enables all Interrupts. Idk if this is necessary */
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
   if (FAST_BUTTON == 1){
-      digitalWrite(G_LED1, LOW);
+    digitalWrite(G_LED1, LOW);
   }
   if (FAST_BUTTON == 0){
-      digitalWrite(G_LED1, HIGH);
+    digitalWrite(G_LED1, HIGH);
   }
+
   digitalWrite(G_LED2, LOW);
   delay(500);
   digitalWrite(G_LED2, HIGH);
   delay(500);
   
- 
-  //Beep();
-  // wait for a second
- // analogWrite(CTL_FAN, 0);
-//  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  new_battery_charge = check_Battery();
+  if (new_battery_charge > battery_charge) {
+    /* Now Charging */
+    reached_15 = False;
+  }  
+  battery_charge = new_battery_charge;
+  if (reached_15) {
+    Beep();
+    delay(1000);
+    analogWrite(CTL_FAN, 50);
+// turn the LED off by making the  voltage LOW
+    digitalWrite(LED, LOW);      
+}
+  if (BATTERY_BUTTON == 1){
+    // call fun for LED  
+  }
 }
 
 /* Get Register from Fuel Gauge */
 uint16_t Get_BatteryValue(uint8_t fuelregister)
 {
-	return Get_BatteryRegister(fuelregister);
+  Get_BatteryRegister(fuelregister);
 }
 
 void Beep(){
-    analogWrite(BUZZER,500);
-    delay(1000);
-    analogWrite(BUZZER, 0);
-    delay(1000);
+  analogWrite(BUZZER,500);
+  delay(1000);
+  analogWrite(BUZZER, 0);
+  delay(1000);
 }
+
 uint16_t Get_BatteryRegister(uint8_t commandAddress)
 {
-    Wire.beginTransmission(0xAA); // transmit to device #44 (0x2c) Send Start Address
-    Wire.write(byte(commandAddress));            // sends instruction byte
-    Wire.write(0xAB);             // sends potentiometer value byte
-    uint8_t lsb = Wire.read();
-    uint8_t msb = Wire.read();
-    uint16_t combined = msb << 8 | lsb;
-    Wire.endTransmission();     // stop transmitting
+  Wire.beginTransmission(0xAA); // transmit to device #44 (0x2c) Send Start Address
+  Wire.write(byte(commandAddress));            // sends instruction byte
+  Wire.write(0xAB);             // sends potentiometer value byte
+  uint8_t lsb = Wire.read();
+  uint8_t msb = Wire.read();
+  uint16_t combined = msb << 8 | lsb;
+  Wire.endTransmission();     // stop transmitting
 
-    return combined;
+  return combined;
 }
-void speedUpInterrupt(){
-    Beep();
-    digitalWrite(G_LED1, 0);
 
-    if (speed > 255){
-        speed = 255;
-    } else {
-        speed = speed + 10;
-    }
-	analogWrite(BUZZER, speed);
-}	
+void speedUpInterrupt(){
+  Beep();
+  digitalWrite(G_LED1, 0);
+
+  if (speed > 255){
+    speed = 255;
+  } else {
+    speed = speed + 10;
+  }
+  analogWrite(BUZZER, speed);
+}    
+
 int state = 0;
 void slowDownInterrupt(){
-    Beep();
-    state = !state;
-    digitalWrite(P1_3, state);
-    if (speed < 0){
-        speed = 0;
-    } else {
-        speed = speed - 10;
+  Beep();
+  state = !state;
+  digitalWrite(P1_3, state);
+  if (speed < 0){
+    speed = 0;
+  } else {
+    speed = speed - 10;
+  }
+  analogWrite(BUZZER, speed);
+}
+
+void LED_Display(int GREEN, int GREEN2, int RED){
+  digitalWrite(G_LED1, GREEN);
+  digitalWrite(G_LED2, GREEN2);
+  digitalWrite(R_LED, RED);
+}
+
+int checkBattery(){
+  int battery_level = Get_BatteryValue();
+  if (battery_level <= 15) {
+    reached_15 = True;
+  }
+  return battery_level;
+}
+
+int Battery_Interrupt_Response(){
+  if (battery_charge <= 100 && battery_charge > 85){
+    LED_Display(HIGH, HIGH, HIGH);
+    delay(5000);
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge <= 85 && battery_charge > 66){
+    for (int i = 0; i < 5; i++){
+          LED_Display(LOW, HIGH, HIGH);
+          delay(500);
+      LED_Display(HIGH, HIGH, HIGH);
+          delay(500);
     }
-	analogWrite(BUZZER, speed);
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge <= 66 && battery_charge > 50){
+      LED_Display(LOW, HIGH, HIGH);
+    delay(5000);
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge <= 50 && battery_charge > 35){
+    for (int i = 0; i < 5; i++){
+LED_Display(LOW, LOW, HIGH);
+          delay(500);
+          LED_Display(LOW, HIGH, HIGH);
+          delay(500);
+    }
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge <= 35 && battery_charge > 25){
+LED_Display(LOW, LOW, HIGH);
+    delay(5000);
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge <= 15 && battery_charge > 5){
+    for (int i = 0; i < 5; i++){
+
+      LED_Display(LOW, LOW, LOW);
+      delay(500);
+      LED_Display(LOW, LOW, HIGH);
+      delay(500);
+    }
+    LED_Display(LOW, LOW, LOW);
+  }
+  else if  (battery_charge < 5)
+    LED_Display(LOW, LOW, LOW);
+      delay(5000);
+  }
+  LED_Display(LOW, LOW, LOW);
 }
-void checkBattery(){
-    Beep();
-//	state = !state;
-//	digitalWrite(LED1, state);
-}
+
 long int timer(int minutes){
-	minutes = minutes * 1;
-	return (1*MHZ / 8) * minutes;
+  minutes = minutes * 1;
+  return (1*MHZ / 8) * minutes;
 }
+
 #pragma vector=TIMERA0_VECTOR
 __interrupt void BatteryStatusTimer(void){
-    P1OUT ^= BIT4;                          // Toggle P1.4 
-
+  P1OUT ^= BIT4;                          // Toggle P1.4
 }
